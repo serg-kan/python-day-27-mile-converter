44ca6f43a5035286580e0d2ff5c907b8d30c61ba
Sergey Kan <sergeykan@ip-10-13-100-203.eu-central-1.compute.internal>
2024-04-04 21:05:23 +0400

    feat: implemented converter

--- main.py
+++ main.py
@@ -3,29 +3,28 @@ from tkinter import *
 
 window = Tk()
 window.title("Mile converter")
-window.minsize(width=500, height=300)
+window['padx'] = 20
+window['pady'] = 20
 
 def button_click(entry_name):
-    my_label["text"] = entry_name.get()
+    label_result["text"] = round((int(entry_name.get()) * 1.60934), 2)
     print("I got clicked")
 
-# Label
-my_label = Label(text="Mile converter", font=("Arial", 24, "bold"))
-# my_label.pack()
+# Layout
+input_mile = Entry(width=10)
+label_mile = Label(text="Mile converter")
+label_is_equal = Label(text="is equal to")
+label_result = Label(text="")
+label_km = Label(text="Km")
 
-# Entry
-input = Entry(width=10)
-# input.pack()
-# input.get()
+button = Button(text="Calculate", command=lambda: button_click(input_mile))
 
-# Button
-button = Button(text="Click Me", command=lambda: button_click(input))
-# button.pack()
-new_button = Button(text="Second button")
 
-my_label.grid(row=1, column=1)
-button.grid(row=2, column=2)
-new_button.grid(row=1, column=3)
-input.grid(row=3, column=4)
+input_mile.grid(row=1, column=2)
+label_mile.grid(row=1, column=3)
+label_is_equal.grid(row=2, column=1)
+label_result.grid(row=2, column=2)
+label_km.grid(row=2, column=3)
+button.grid(row=3, column=2)
 
 window.mainloop()
